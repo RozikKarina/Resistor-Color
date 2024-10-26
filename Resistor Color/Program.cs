05443f56b4b9a0e8657d6232564fabbd8591e29c
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 02:33:34 +0300

    Добавьте файлы проекта.

---
+++ Resistor Color/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    private static readonly string[] colorList =
+    {
+        "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"
+    };
+
+    public static int ColorCode(string color)
+    {
+        // Знаходимо індекс кольору, який відповідає його числовому значенню
+        return Array.IndexOf(colorList, color);
+    }
+
+    public static string[] Colors()
+    {
+        // Повертаємо масив всіх кольорів
+        return colorList;
+    }
+}
